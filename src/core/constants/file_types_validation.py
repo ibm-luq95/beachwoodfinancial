55e51c906faa5b7db50c7cef9db80f90761b5cc6
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-05-17 17:43:42 +0300

    feat: file validator object for models

---
+++ src/core/constants/file_types_validation.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-#
+IMGS_AND_DOCS_FT = (
+    "image/png",
+    "image/jpeg",
+    "text/csv",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.oasis.opendocument.spreadsheet",
+    "application/pdf",
+    "text/plain",
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.oasis.opendocument.text",
+)
+
+IMAGES_FT = (
+    "image/png",
+    "image/jpeg",
+    "image/jpg",
+)
+
+DOCUMENTS_FT = (
+    "text/csv",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.oasis.opendocument.spreadsheet",
+    "application/pdf",
+    "text/plain",
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.oasis.opendocument.text",
+)
