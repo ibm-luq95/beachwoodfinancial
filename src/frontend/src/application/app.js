d8a61913b477efb05262fbdce4a471f194207efd
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-05-17 17:33:22 +0300

    feat: import new packages

--- src/frontend/src/application/app.js
+++ src/frontend/src/application/app.js
@@ -3,6 +3,7 @@ import "../styles/index.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "preline";
+import "css.gg/icons/all.css";
 
 window.document.addEventListener("DOMContentLoaded", function () {
   window.console.log("dom ready 1");
