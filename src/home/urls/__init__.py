62f0ead66fc0011256eba8d7c4b5e37322405b19
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-05-17 17:41:43 +0300

    new: add new urls to new views

--- src/home/urls/__init__.py
+++ src/home/urls/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-#
-from django.urls import path, include
+from django.urls import path
 
-from home.views import LandingPageView, AboutView
+from home.views import LandingPageView, AboutView, FAQView, PricesView, ContactView
 
 app_name = "home"
 
 urlpatterns = [
-    path("", LandingPageView.as_view(), name="landing-page"),
-    path("about", AboutView.as_view(), name="about-page"),
+    path("", LandingPageView.as_view(), name="landing"),
+    path("about", AboutView.as_view(), name="about"),
+    path("faq", FAQView.as_view(), name="faq"),
+    path("prices", PricesView.as_view(), name="prices"),
+    path("contact", ContactView.as_view(), name="contact"),
 ]
