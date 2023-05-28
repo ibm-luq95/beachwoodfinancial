db4fb95fc1b202032308c441960863b569db8395
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-05-28 21:52:53 +0300

    new: extend category model with GeneralStatusFieldMixin

--- src/client_category/models/category.py
+++ src/client_category/models/category.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-#
+
 from django.db import models
 from django.utils.translation import gettext as _
 
-from core.models.mixins import BaseModelMixin, StrModelMixin
+from core.models.mixins import BaseModelMixin, StrModelMixin, GeneralStatusFieldMixin
 
 
-class ClientCategory(BaseModelMixin, StrModelMixin):
+class ClientCategory(BaseModelMixin, GeneralStatusFieldMixin, StrModelMixin):
     name = models.CharField(_("name"), max_length=50, db_index=True)
