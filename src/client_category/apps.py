741e27dd8a35926d98ca4db5845ef6467c93c46e
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-05-28 21:53:20 +0300

    fix: fix verbose_name

--- src/client_category/apps.py
+++ src/client_category/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ClientCategoryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "client_category"
+    verbose_name = "Client Categories"
