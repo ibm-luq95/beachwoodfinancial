abb46394dae4115ef3d000c24d3722fa7de97170
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-06-10 12:56:03 +0300

    fix: fix set variable template tag

--- src/core/templatetags/core_tags.py
+++ src/core/templatetags/core_tags.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-#
 from datetime import datetime
 from typing import Optional
+import re
 
 from django import template
 from django.core.paginator import Paginator
@@ -29,16 +30,33 @@ def now_timestamp(date_and_time: Optional[str] = None) -> timezone:
     return timestamp
 
 
-class MyCustomTagNode(Node):
-    def __init__(self, value):
-        self.value = value
+class SetVarNode(template.Node):
+    def __init__(self, new_val, var_name):
+        self.new_val = new_val
+        self.var_name = var_name
 
     def render(self, context):
-        print("##############################3")
-        context["my_variable"] = self.value
+        context[self.var_name] = self.new_val
         return ""
 
 
-@register.simple_tag(name="my_custom_tag")
-def my_custom_tag(value):
-    return MyCustomTagNode(value)
+@register.tag
+def setvar(parser, token):
+    # {% setvar "a string" as new_template_var %}
+    # This version uses a regular expression to parse tag contents.
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "%r tag requires arguments" % token.contents.split()[0]
+        )
+    m = re.search(r"(.*?) as (\w+)", arg)
+    if not m:
+        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
+    new_val, var_name = m.groups()
+    if not (new_val[0] == new_val[-1] and new_val[0] in ('"', "'")):
+        raise template.TemplateSyntaxError(
+            "%r tag's argument should be in quotes" % tag_name
+        )
+    return SetVarNode(new_val[1:-1], var_name)
