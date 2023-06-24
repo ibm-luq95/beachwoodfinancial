ef46daf84ab6d4b3d24ac3913df310b1538a3965
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-06-24 19:48:49 +0300

    new: create new template tag bw_truncate_titles

--- src/core/templatetags/string_helpers_tags.py
+++ src/core/templatetags/string_helpers_tags.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-#
 import stringcase
+import textwrap
 from django import template
 from django.template.defaultfilters import stringfilter
+
+from core.constants.general import DEFAULT_TEMPLATE_TABLE_LIST_TRUNCATED_STRING
 
 register = template.Library()
 
@@ -16,3 +19,11 @@ def to_title_case(text: str) -> str:
 @stringfilter
 def replace_string(text: str) -> str:
     return text.replace("_", " ")
+
+
+@register.filter(name="bw_truncate_titles")
+@stringfilter
+def bw_truncate_titles(text: str) -> str:
+    return textwrap.shorten(
+        text, width=DEFAULT_TEMPLATE_TABLE_LIST_TRUNCATED_STRING, placeholder="..."
+    )
