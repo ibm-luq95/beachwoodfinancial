d36dbda3676fef8e9a1fde7553347ea2b7a9bc51
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-06-24 19:47:30 +0300

    doc: add usage syntax

--- src/bw_ui_components/templatetags/inputs/select.py
+++ src/bw_ui_components/templatetags/inputs/select.py
@@ -9,13 +9,14 @@ register = template.Library()
 
 @register.inclusion_tag("bw_ui_components/inputs/select.html", takes_context=True)
 def bw_select(context, *args, **kwargs) -> dict:
+    # Syntax: {% bw_select options='{"one": "One", "tow": "tow", "three": "three"}' %}
     context_data = get_request_context(context, kwargs)
     options = kwargs.get("options")
     try:
         if options:
             options = json.loads(options)
             kwargs.update({"options": options})
-        debugging_print(options)
+        # debugging_print(options)
     except json.JSONDecodeError as jerror:
         raise ValueError("Input data is not a valid JSON object: {}".format(str(jerror)))
     return {**context_data, **kwargs}
