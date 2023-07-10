d5a1c72b3a070a1cedfdf2435b000200acc5c335
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-07-10 11:32:20 +0300

    refact: refactor init site settings

--- src/site_settings/management/commands/init_site_settings.py
+++ src/site_settings/management/commands/init_site_settings.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-#
-
+from decouple import Config, RepositoryEnv
+from django.conf import settings
+from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.utils.translation import gettext as _
 
+from core.constants.identity import BWIdentity
+from core.constants.site_settings import SITE_SETTINGS_DB_SLUG, APP_CONFIGS_DB_SLUG
 from core.management.mixins import CommandStdOutputMixin
 from site_settings.models import SiteSettings, ApplicationConfigurations
 
@@ -15,21 +19,26 @@ class Command(BaseCommand, CommandStdOutputMixin):
         try:
             with transaction.atomic():
                 data = {
-                    "slug": "web app",
-                    "email": "beach_wood_financial@email.com",
-                    "name": "Beach Wood Financial",
-                    "url": "https://app.beachwoodfinancial.com/",
-                    "title": _(
-                        "Beach Wood Financial - Manage bookkeeping clients with jobs"
-                    ),
-                    "manager_name": "Albert Salazar",
+                    "slug": SITE_SETTINGS_DB_SLUG,
+                    "email": BWIdentity.email_address,
+                    "name": BWIdentity.name,
+                    "url": BWIdentity.url,
+                    "title": BWIdentity.title,
+                    "manager_name": BWIdentity.manager_name,
+                    "description": BWIdentity.description
                 }
+                env_file_path = settings.BASE_DIR / ".env" / ".env_dev"
+                config = Config(RepositoryEnv(env_file_path))
+                site_domain = config("SITE_DOMAIN", cast=str)
+                site_name = config("SITE_NAME", cast=str)
+                site = Site.objects.get(domain=site_domain)
+                data.update({"site": site})
                 # site_settings = SiteSettings.objects.select_related().create(**data)
                 site_settings, created = SiteSettings.objects.get_or_create(
-                    slug="web-app", defaults=data
+                    slug=SITE_SETTINGS_DB_SLUG, defaults=data
                 )
                 app_configs, created2 = ApplicationConfigurations.objects.get_or_create(
-                    slug="app-configs"
+                    slug=APP_CONFIGS_DB_SLUG
                 )
                 self.stdout_output("success", site_settings)
                 self.stdout_output("success", app_configs)
