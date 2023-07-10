243197cfa7bcf679e669cc73e088ba39930a5d46
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-07-10 11:30:43 +0300

    new: identity class

---
+++ src/core/constants/identity.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-#
+from django.utils.translation import gettext as _
+
+
+class BWIdentity:
+    email_address = "beach_wood_financial@email.com"
+    name = _("Beach Wood Financial")
+    url = "https://app.beachwoodfinancial.com/"
+    title = _("Beach Wood Financial - Manage bookkeeping clients with jobs")
+    manager_name = _("Albert Salazar")
+    description = _("")
