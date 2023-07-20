dd281574a61b976a1ec18351279a24ac9050d482
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-07-20 22:46:21 +0300

    docs: add docstring and use new .env file name

--- src/core/utils/grab_env_file.py
+++ src/core/utils/grab_env_file.py
@@ -4,7 +4,20 @@ from django.conf import settings
 from decouple import Config, RepositoryEnv
 
 
-def grab_env_file(env_file_name: str = ".env_dev") -> Config:
+def grab_env_file(env_file_name: str = ".env") -> Config:
+    """
+    This function will use only in django project, views, models,..etc
+    Don't ues it outside the django project
+    Parameters
+    ----------
+    env_file_name: str = this will be the .env file name, in case using files other than .env file (default)
+
+    Returns
+    -------
+        Config: Config = config object of .env variables
+    """
     env_file_path = settings.BASE_DIR / ".env" / env_file_name
+    if env_file_path.exists() is False:
+        raise Exception(".env file not exists!!!")
     config = Config(RepositoryEnv(env_file_path))
     return config
