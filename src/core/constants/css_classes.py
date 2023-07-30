bdc31cb52da44b222b141743b9aa25085d8cf61a
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-07-30 12:34:48 +0300

    new css class for tables

--- src/core/constants/css_classes.py
+++ src/core/constants/css_classes.py
@@ -11,8 +11,10 @@ BW_TABLE_LIST_SORT_CLASS = "table-sort"
 BW_PRELINE_TEXT_INPUT_CSS_CLASSES = (
     f"{BW_BASE_INPUT_CSS_CLASSES} {BW_DISABLED_DISABLED_INPUTS_CSS_CLASSES} py-2 px-3 "
     # f"pr-11 block w-full shadow-sm text-sm rounded-lg "
-    f"block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 "
-    f"dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 shadow-sm text-sm rounded-lg"
+    f"block w-full border-gray-200 rounded-md text-sm focus:border-blue-500"
+    f" focus:ring-blue-500 "
+    f"dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 shadow-sm text-sm"
+    f" rounded-lg"
 )
 BW_PRELINE_INPUT_ERROR_CLASSES = (
     f" border-red-500 text-sm focus:border-red-500 focus:ring-red-500 dark:bg-gray-800 "
@@ -20,15 +22,16 @@ BW_PRELINE_INPUT_ERROR_CLASSES = (
 )
 BW_PRELINE_SELECT_INPUT_CSS_CLASSES = (
     f"{BW_FULL_INPUT_CSS_CLASSES} py-3 px-4 pr-9 block w-full border-gray-200 "
-    f"rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 "
-    f"dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+    "rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 "
+    "dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
 )
 BW_PRELINE_BASE_BUTTON = (
-    f"{BW_BASE_INPUT_CSS_CLASSES} {BW_DISABLED_DISABLED_INPUTS_CSS_CLASSES} inline-flex justify-center items-center "
-    f"gap-2 rounded-md border border-transparent transition-all text-sm font-semibold "
+    f"{BW_BASE_INPUT_CSS_CLASSES} {BW_DISABLED_DISABLED_INPUTS_CSS_CLASSES} inline-flex"
+    " justify-center items-center gap-2 rounded-md border border-transparent"
+    " transition-all text-sm font-semibold "
 )
 BW_PRELINE_BASE_TABLE = (
-    "table-sort min-w-full divide-y divide-gray-200 dark:divide-gray-700"
+    "table-sort cells-sort min-w-full divide-y divide-gray-200 dark:divide-gray-700"
 )
 BW_PRELINE_ROUNDED_CLASS = "rounded-full"
 BW_PRELINE_BLOCK_CLASS = "w-full"
@@ -39,18 +42,19 @@ BW_PRELINE_INPUT_SUCCESS_STATE = (
     "border-green-500 rounded-md text-sm focus:border-green-500 focus:ring-green-500"
 )
 BW_PRELINE_FILTER_TEXT_INPUT = (
-    f"{BW_FULL_INPUT_CSS_CLASSES} py-2 px-3 pl-11 block w-full border-gray-200 shadow-sm rounded-md text-sm focus:z-10 "
-    f"focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 "
-    f"dark:text-gray-400"
+    f"{BW_FULL_INPUT_CSS_CLASSES} py-2 px-3 pl-11 block w-full border-gray-200 shadow-sm"
+    " rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+    " dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
 )
 BW_PRELINE_FILTER_SELECT_INPUT_DEFAULT = (
-    f"{BW_FULL_INPUT_CSS_CLASSES} py-2 pl-4 pr-0 block w-full border-gray-200 rounded-md text-sm "
-    f"focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 "
-    f"dark:border-gray-700 dark:text-gray-400"
+    f"{BW_FULL_INPUT_CSS_CLASSES} py-2 pl-4 pr-0 block w-full border-gray-200 rounded-md"
+    " text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900"
+    " dark:border-gray-700 dark:text-gray-400"
 )
 BW_PRELINE_CHECKBOX_CSS_CLASSES = (
-    f"{BW_FULL_INPUT_CSS_CLASSES} shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none "
-    f"focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 "
-    f"dark:checked:bg-blue-500 dark:checked:border-blue-500 "
-    f"dark:focus:ring-offset-gray-800"
+    f"{BW_FULL_INPUT_CSS_CLASSES} shrink-0 mt-0.5 border-gray-200 rounded text-blue-600"
+    " pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700"
+    " dark:checked:bg-blue-500 dark:checked:border-blue-500"
+    " dark:focus:ring-offset-gray-800"
 )
+BW_PRELINE_ANCHOR_TAG_DEFAULT_COLOR = "text-blue-500"
