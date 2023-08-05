08c794305f2f3933dfa8d495a259032f134b11cc
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-05 18:27:12 +0300

    new: create new model: Profile

---
+++ src/beach_wood_user/admin/profile.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-#
+from django.contrib import admin
+
+from beach_wood_user.models import Profile
+from core.admin import BWBaseAdminModelMixin
+
+
+@admin.register(Profile)
+class ProfileAdmin(BWBaseAdminModelMixin):
+    pass
--- src/beach_wood_user/models/__init__.py
+++ src/beach_wood_user/models/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-#
 from .beach_wood_user import BWUser
+from .profile import Profile
