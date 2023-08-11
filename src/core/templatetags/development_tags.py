c994479bc73523a8fffe85881ee16201512b2a2d
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-12 01:44:44 +0300

    feat: create new template tag

--- src/core/templatetags/development_tags.py
+++ src/core/templatetags/development_tags.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-#
 from django import template
 
+from core.utils import debugging_print
+
 register = template.Library()
 
 
 @register.filter(name="get_var_type")
 def get_var_type(var) -> str:
     return type(var)
+
+
+@register.filter(name="dir_var")
+def dir_var(var):
+    debugging_print(dir(var))
