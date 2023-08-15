34d13bd7a85682b1a864179f7f10bc143d52a482
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-15 20:58:18 +0300

    new: create new django form MiniSpecialAssignmentForm

---
+++ src/special_assignment/forms/mini_form.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-#
+from django import forms
+from django.utils.translation import gettext as _
+
+from core.constants.file_types_validation import IMAGES_AND_DOCS_FT
+from core.forms.mixins.base_form_mixin import BWBaseFormMixin
+from core.utils import FileValidator
+
+file_validator = FileValidator(max_size=1024 * 1000, content_types=IMAGES_AND_DOCS_FT)
+
+
+class MiniSpecialAssignmentForm(BWBaseFormMixin):
+    def __init__(self, *args, **kwargs):
+        super(BWBaseFormMixin, self).__init__(*args, **kwargs)
+        self.fields["assigned_by"].label = ""
+
+    title = forms.CharField(label=_("Title"), required=True)
+    body = forms.CharField(label=_("Body"), required=True, widget=forms.Textarea)
+    attachment = forms.FileField(
+        label=_("Attachment"), required=False, validators=[file_validator]
+    )
+    assigned_by = forms.UUIDField(widget=forms.HiddenInput)
+
