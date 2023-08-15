6addb1d17a903bfb2de37feb374b0018cf0e3a7e
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-15 21:04:58 +0300

    new: add job field and fix is seen label

--- src/special_assignment/models/special_assignment.py
+++ src/special_assignment/models/special_assignment.py
@@ -13,6 +13,7 @@ from core.models.mixins import (
     StrModelMixin,
 )
 from core.utils import FileValidator
+from job.models import JobProxy
 from special_assignment.models.manager import SpecialAssignmentsManager
 
 file_validator = FileValidator(max_size=1024 * 1000, content_types=IMAGES_AND_DOCS_FT)
@@ -22,7 +23,18 @@ class SpecialAssignment(
     BaseModelMixin, StartAndDueDateMixin, TeamMembersMixin, StrModelMixin
 ):
     client = models.ForeignKey(
-        to=ClientProxy, on_delete=models.PROTECT, related_name="special_assignments"
+        to=ClientProxy,
+        on_delete=models.PROTECT,
+        related_name="special_assignments",
+        null=True,
+        blank=True,
+    )
+    job = models.ForeignKey(
+        to=JobProxy,
+        on_delete=models.PROTECT,
+        related_name="special_assignments",
+        null=True,
+        blank=True,
     )
     title = models.CharField(_("title"), max_length=100, null=False, blank=False)
     body = models.TextField(_("body"))
@@ -40,7 +52,7 @@ class SpecialAssignment(
         validators=[file_validator],
     )
     notes = models.TextField(_("notes"), null=True, blank=True)
-    is_seen = models.BooleanField(_("is_seen"), default=False)
+    is_seen = models.BooleanField(_("is seen"), default=False)
     assigned_by = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
