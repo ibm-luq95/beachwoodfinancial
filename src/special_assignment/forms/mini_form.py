c852c942fd05be11b743e779eef109528bdf3a96
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-17 20:24:40 +0300

    fix: use new form renderer BWJSModalFormRendererMixin

--- src/special_assignment/forms/mini_form.py
+++ src/special_assignment/forms/mini_form.py
@@ -4,12 +4,13 @@ from django.utils.translation import gettext as _
 
 from core.constants.file_types_validation import IMAGES_AND_DOCS_FT
 from core.forms.mixins.base_form_mixin import BWBaseFormMixin
+from core.forms.mixins.js_modal_form_renderer_mixin import BWJSModalFormRendererMixin
 from core.utils import FileValidator
 
 file_validator = FileValidator(max_size=1024 * 1000, content_types=IMAGES_AND_DOCS_FT)
 
 
-class MiniSpecialAssignmentForm(BWBaseFormMixin):
+class MiniSpecialAssignmentForm(BWJSModalFormRendererMixin, BWBaseFormMixin):
     def __init__(self, *args, **kwargs):
         super(BWBaseFormMixin, self).__init__(*args, **kwargs)
         self.fields["assigned_by"].label = ""
@@ -20,4 +21,3 @@ class MiniSpecialAssignmentForm(BWBaseFormMixin):
         label=_("Attachment"), required=False, validators=[file_validator]
     )
     assigned_by = forms.UUIDField(widget=forms.HiddenInput)
-
