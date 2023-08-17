129d309a0a2f247db2b6d8c628b65a8607cad6d8
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-17 20:27:34 +0300

    fix: add new fields

--- src/discussion/serializers/discussion.py
+++ src/discussion/serializers/discussion.py
@@ -2,12 +2,19 @@
 from rest_framework import serializers
 
 from discussion.models import DiscussionProxy
+from job.models import JobProxy
 from special_assignment.models import SpecialAssignment
 
 
 class DiscussionSerializer(serializers.ModelSerializer):
     special_assignment = serializers.PrimaryKeyRelatedField(
-        queryset=SpecialAssignment.objects.all(), many=False
+        queryset=SpecialAssignment.objects.all(), many=False, required=False
+    )
+    replies = serializers.PrimaryKeyRelatedField(
+        queryset=DiscussionProxy.objects.all(), many=False, required=False
+    )
+    job = serializers.PrimaryKeyRelatedField(
+        queryset=JobProxy.objects.all(), many=False, required=False
     )
     replies = serializers.PrimaryKeyRelatedField(
         queryset=DiscussionProxy.objects.all(), many=False, required=False
