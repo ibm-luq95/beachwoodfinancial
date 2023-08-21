42c94a47b0d685be030ab796062ee9871204810c
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-21 14:31:54 +0300

    fix: fix form

--- src/client_account/forms/client_account.py
+++ src/client_account/forms/client_account.py
@@ -1,16 +1,37 @@
 # -*- coding: utf-8 -*-#
+from typing import Optional
+
 from django import forms
 
 from client_account.models import ClientAccount
 from core.constants.form import EXCLUDED_FIELDS
 from core.forms import BaseModelFormMixin
+from core.forms.mixins.set_field_to_hidden import SetFieldsInputsHiddenMixin
 from core.forms.widgets import BWPasswordInputWidget
 from core.utils import PasswordHasher
 
 
-class ClientAccountForm(BaseModelFormMixin):
-    def __init__(self, client=None, is_update=False, updated_object=None, *args, **kwargs):
+class ClientAccountForm(BaseModelFormMixin, SetFieldsInputsHiddenMixin):
+    field_order = [
+        "service_name",
+        "account_name",
+        "account_email",
+        "account_url",
+        "account_username",
+        "account_password",
+    ]
+
+    def __init__(
+        self,
+        client=None,
+        is_update=False,
+        updated_object=None,
+        hidden_inputs: Optional[dict] = None,
+        *args,
+        **kwargs,
+    ):
         super(ClientAccountForm, self).__init__(*args, **kwargs)
+        SetFieldsInputsHiddenMixin.__init__(self, hidden_inputs=hidden_inputs)
         self.is_update = is_update
         self.updated_object = updated_object
         if client is not None:
