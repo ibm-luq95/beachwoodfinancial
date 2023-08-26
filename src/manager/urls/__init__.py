fda12610a09c4bbf780506c382cf042b9ee153c0
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-26 18:34:58 +0300

    manager urls

--- src/manager/urls/__init__.py
+++ src/manager/urls/__init__.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-#
+from django.urls import path, include
+from manager.views import (
+    ManagerDeleteView,
+    ManagerUpdateView,
+    ManagerCreateView,
+    ManagerListView,
+    ManagerDetailsView,
+)
+
+app_name = "managers"
+
+urlpatterns = [
+    path("", ManagerListView.as_view(), name="list"),
+    path("create", ManagerCreateView.as_view(), name="create"),
+    path("update/<uuid:pk>", ManagerUpdateView.as_view(), name="update"),
+    path("delete/<uuid:pk>", ManagerDeleteView.as_view(), name="delete"),
+    path("<uuid:pk>", ManagerDetailsView.as_view(), name="details"),
+]
