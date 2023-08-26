e7120a56fb6fc8594ee296fee083a4808a6ec782
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-26 18:31:27 +0300

    fix: change class name

--- src/core/forms/widgets/rich_html_editor.py
+++ src/core/forms/widgets/rich_html_editor.py
@@ -7,5 +7,5 @@ from django import forms
 class RichHTMLEditorWidget(forms.widgets.Textarea):
     def __init__(self, attrs: Optional[dict] = None, *args, **kwargs):
         attrs = attrs or {}
-        attrs.update({"class": "wyswyg-textarea"})
+        attrs.update({"class": "rich-editor"})
         super(RichHTMLEditorWidget, self).__init__(attrs)
