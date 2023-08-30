7883da243214e8a12e2e0c846c90b8999a36263f
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-30 17:17:38 +0300

    feat: use new form mime types for all file upload inputs

--- src/discussion/forms/mini_form.py
+++ src/discussion/forms/mini_form.py
@@ -5,12 +5,25 @@ from django import forms
 from django.utils.translation import gettext as _
 
 from core.constants.file_types_validation import IMAGES_AND_DOCS_FT
+from core.constants.form import (
+    PDF_MIME_TYPE,
+    DOCX_MIME_TYPE,
+    DOC_MIME_TYPE,
+    CSV_MIME_TYPE,
+    PNG_MIME_TYPE,
+    JPEG_MIME_TYPE,
+    JPG_MIME_TYPE,
+    WEBP_MIME_TYPE,
+    AVIF_MIME_TYPE,
+    PPT_MIME_TYPE,
+    PPTX_MIME_TYPE,
+)
 from core.forms.mixins.base_form_mixin import BWBaseFormMixin
 from core.forms.mixins.js_modal_form_renderer_mixin import BWJSModalFormRendererMixin
 from core.forms.mixins.remove_fields_mixin import RemoveFieldsMixin
 from core.forms.mixins.staff_member_form_mixin import BWStaffMemberFormMixin
 from core.forms.widgets import RichHTMLEditorWidget
-from core.utils import FileValidator, debugging_print
+from core.utils import FileValidator
 from job.models import JobProxy
 from special_assignment.models import SpecialAssignmentProxy
 
@@ -24,6 +37,16 @@ class DiscussionMiniForm(
         super(BWBaseFormMixin, self).__init__(*args, **kwargs)
         BWStaffMemberFormMixin.__init__(self, *args, **kwargs)
         RemoveFieldsMixin.__init__(self, removed_fields=removed_fields)
+        self.fields.get("attachment").widget.attrs.update(
+            {
+                "accept": (
+                    f"{PDF_MIME_TYPE}, {DOCX_MIME_TYPE}, {DOC_MIME_TYPE}, {CSV_MIME_TYPE},"
+                    f" {PNG_MIME_TYPE}, {JPEG_MIME_TYPE}, {JPG_MIME_TYPE},"
+                    f" {PPTX_MIME_TYPE}, {PPT_MIME_TYPE}, {AVIF_MIME_TYPE},"
+                    f" {WEBP_MIME_TYPE}"
+                )
+            }
+        )
 
     body = forms.CharField(label=_("Body"), required=True, widget=RichHTMLEditorWidget)
     attachment = forms.FileField(
--- src/special_assignment/forms/mini_form.py
+++ src/special_assignment/forms/mini_form.py
@@ -3,6 +3,19 @@ from django import forms
 from django.utils.translation import gettext as _
 
 from core.constants.file_types_validation import IMAGES_AND_DOCS_FT
+from core.constants.form import (
+    PDF_MIME_TYPE,
+    DOCX_MIME_TYPE,
+    DOC_MIME_TYPE,
+    CSV_MIME_TYPE,
+    PNG_MIME_TYPE,
+    JPEG_MIME_TYPE,
+    JPG_MIME_TYPE,
+    WEBP_MIME_TYPE,
+    AVIF_MIME_TYPE,
+    PPT_MIME_TYPE,
+    PPTX_MIME_TYPE,
+)
 from core.forms.mixins.base_form_mixin import BWBaseFormMixin
 from core.forms.mixins.js_modal_form_renderer_mixin import BWJSModalFormRendererMixin
 from core.forms.widgets import RichHTMLEditorWidget
@@ -15,6 +28,16 @@ class MiniSpecialAssignmentForm(BWJSModalFormRendererMixin, BWBaseFormMixin):
     def __init__(self, *args, **kwargs):
         super(BWBaseFormMixin, self).__init__(*args, **kwargs)
         self.fields["assigned_by"].label = ""
+        self.fields.get("attachment").widget.attrs.update(
+            {
+                "accept": (
+                    f"{PDF_MIME_TYPE}, {DOCX_MIME_TYPE}, {DOC_MIME_TYPE}, {CSV_MIME_TYPE},"
+                    f" {PNG_MIME_TYPE}, {JPEG_MIME_TYPE}, {JPG_MIME_TYPE},"
+                    f" {PPTX_MIME_TYPE}, {PPT_MIME_TYPE}, {AVIF_MIME_TYPE},"
+                    f" {WEBP_MIME_TYPE}"
+                )
+            }
+        )
 
     title = forms.CharField(label=_("Title"), required=True)
     body = forms.CharField(label=_("Body"), required=True, widget=RichHTMLEditorWidget)
