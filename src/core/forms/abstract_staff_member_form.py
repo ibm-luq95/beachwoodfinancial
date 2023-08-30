93b1d934350c6f800631936e9896fc9a6f81d454
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-08-30 17:16:31 +0300

    feat: create new method, and set user and profile data attribute

--- src/core/forms/abstract_staff_member_form.py
+++ src/core/forms/abstract_staff_member_form.py
@@ -1,22 +1,52 @@
 # -*- coding: utf-8 -*-#
+from typing import Optional
+
 from django import forms
 from django.utils.translation import gettext as _
 
 from core.choices import BeachWoodUserTypeEnum
+from core.forms.mixins.remove_fields_mixin import RemoveFieldsMixin
 from core.forms.widgets import RichHTMLEditorWidget, BWPasswordInputWidget
+from core.utils import debugging_print
 
 
-class AbstractStaffMemberForm(forms.Form):
-    def __init__(self, user_type: str, *args, **kwargs):
+class AbstractStaffMemberForm(forms.Form, RemoveFieldsMixin):
+    def __init__(
+        self, user_type: str, removed_fields: Optional[list] = None, *args, **kwargs
+    ):
         super().__init__(*args, **kwargs)
+        RemoveFieldsMixin.__init__(self, removed_fields=removed_fields)
         self.fields.get("user_type").initial = user_type
+        if self.fields.get("password"):
+            self.fields.get("password").widget.attrs["data-block-section"] = "user"
+        if self.fields.get("confirm_password"):
+            self.fields.get("confirm_password").widget.attrs["data-block-section"] = "user"
+        self.fields.get("user_type").widget.attrs["data-block-section"] = "user"
+        self.fields.get("email").widget.attrs["data-block-section"] = "user"
+        self.fields.get("first_name").widget.attrs["data-block-section"] = "user"
+        self.fields.get("last_name").widget.attrs["data-block-section"] = "user"
+        self.fields.get("user_type").widget.attrs["data-block-section"] = "user"
+        if self.fields.get("profile_picture"):
+            self.fields.get("profile_picture").widget.attrs["data-block-section"] = "profile"
+        self.fields.get("facebook").widget.attrs["data-block-section"] = "profile"
+        self.fields.get("twitter").widget.attrs["data-block-section"] = "profile"
+        self.fields.get("github").widget.attrs["data-block-section"] = "profile"
+        self.fields.get("linkedin").widget.attrs["data-block-section"] = "profile"
+        self.fields.get("instagram").widget.attrs["data-block-section"] = "profile"
 
-    field_order = ["first_name", "last_name", "email", "password", "confirm_password"]
+    field_order = [
+        "first_name",
+        "last_name",
+        "email",
+        "password",
+        "confirm_password",
+        "profile_picture",
+    ]
     password = forms.CharField(
-        label=_("Password"), required=True, widget=BWPasswordInputWidget
+        label=_("Password"), required=False, widget=BWPasswordInputWidget
     )
     confirm_password = forms.CharField(
-        label=_("Confirm Password"), required=True, widget=BWPasswordInputWidget
+        label=_("Confirm Password"), required=False, widget=BWPasswordInputWidget
     )
     email = forms.EmailField(label=_("Email Address"), required=True)
     first_name = forms.CharField(label=_("First Name"), required=True)
@@ -39,5 +69,22 @@ class AbstractStaffMemberForm(forms.Form):
         password = self.cleaned_data.get("password")
         confirm_password = self.cleaned_data.get("confirm_password")
         if password != confirm_password:
-            raise forms.ValidationError(_("Password confirmation not match!"), code="invalid")
+            raise forms.ValidationError(
+                _("Password confirmation not match!"), code="invalid"
+            )
         return confirm_password
+
+    def split_user_profiles_inputs(self, excluded_fields: Optional[list] = []) -> dict:
+        inputs = dict()
+        inputs["user"] = dict()
+        inputs["profile"] = dict()
+        for field_name, field in self.fields.items():
+            # debugging_print(dir(field.bound_data()))
+            if field_name not in excluded_fields:
+                if field.widget.attrs.get("data-block-section") == "user":
+                    inputs["user"][field_name] = self.cleaned_data.get(field_name)
+                elif field.widget.attrs.get("data-block-section") == "profile":
+                    inputs["profile"][field_name] = self.cleaned_data.get(field_name)
+
+        # debugging_print(inputs)
+        return inputs
