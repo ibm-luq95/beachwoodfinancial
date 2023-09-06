ce7f997ec964a3a3a587d75f9231847555e9e7b5
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-09-06 20:04:10 +0300

    fix: add encrypt in save method instead of client account form

--- src/client_account/models/client_account.py
+++ src/client_account/models/client_account.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 
 from core.choices import ClientAccountStatusEnum, ServiceNameEnum
 from core.models.mixins import BaseModelMixin
-from core.utils import PasswordHasher
+from core.utils import PasswordHasher, debugging_print
 
 from client.models import ClientProxy
 
@@ -48,9 +48,6 @@ class ClientAccount(BaseModelMixin):
     def __str__(self) -> str:
         return f"{self.account_name}"
 
-    # def get_absolute_url(self):
-    #     return reverse("manager:client_account:details", kwargs={"pk": self.pk})
-
     @property
     def decrypted_account_password(self) -> str | None:
         if not self.account_password:
@@ -62,4 +59,8 @@ class ClientAccount(BaseModelMixin):
     def save(self, *args, **kwargs):
         if self.service_name != "":
             self.is_services = True
+        if self.decrypted_account_password:
+            self.account_password = PasswordHasher.encrypt(self.decrypted_account_password)
+        else:
+            self.account_password = PasswordHasher.encrypt(self.account_password)
         super(ClientAccount, self).save(*args, **kwargs)
