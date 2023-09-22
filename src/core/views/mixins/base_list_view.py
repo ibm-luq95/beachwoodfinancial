00ef77fac837a28c3b0082e5202cc1d775dce454
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-09-22 21:50:18 +0300

    fix: fix bug
    fix bug when view has model attribute

--- src/core/views/mixins/base_list_view.py
+++ src/core/views/mixins/base_list_view.py
@@ -20,19 +20,26 @@ class BWBaseListViewMixin:
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         context.setdefault("is_show_created_at", IS_SHOW_CREATED_AT)
-        if hasattr(self.model, "_meta"):
-            context.setdefault("app_label", self.model._meta.app_label)
-            # context.setdefault("app_label", self.model._meta.model_name)
-        if hasattr(self, "model"):
-            excluded_fields = ["id", "updated_at", "metadata", "deleted_at", "is_deleted"]
-            # for f in getattr(self.model, "_meta").fields:
-            # debugging_print(field.is_relation and field.many_to_many and field.related_name)
-            # debugging_print(f.is_relation)
-            names_list = [field.name for field in getattr(self.model, "_meta").fields]
-            new_list = []
-            for name in names_list:
-                if name not in excluded_fields:
-                    new_list.append(stringcase.sentencecase(name.upper()))
+        if hasattr(self, "modal"):
+            if hasattr(self.model, "_meta"):
+                context.setdefault("app_label", self.model._meta.app_label)
+                # context.setdefault("app_label", self.model._meta.model_name)
+            if hasattr(self, "model"):
+                excluded_fields = [
+                    "id",
+                    "updated_at",
+                    "metadata",
+                    "deleted_at",
+                    "is_deleted",
+                ]
+                # for f in getattr(self.model, "_meta").fields:
+                # debugging_print(field.is_relation and field.many_to_many and field.related_name)
+                # debugging_print(f.is_relation)
+                names_list = [field.name for field in getattr(self.model, "_meta").fields]
+                new_list = []
+                for name in names_list:
+                    if name not in excluded_fields:
+                        new_list.append(stringcase.sentencecase(name.upper()))
 
-            context.setdefault("fields", new_list)
+                context.setdefault("fields", new_list)
         return context
