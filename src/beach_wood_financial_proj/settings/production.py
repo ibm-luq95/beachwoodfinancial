13e2b02cd1f78ed7e4c26eb8076c172ebab13710
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-10-13 17:22:20 +0300

    restore production settings

--- src/beach_wood_financial_proj/settings/production.py
+++ src/beach_wood_financial_proj/settings/production.py
@@ -1,7 +1,6 @@
 from .base import *
 
-# DEBUG = config("DEBUG", cast=bool)
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 # Database configurations
 DATABASES = {
@@ -19,10 +18,10 @@ DATABASES = {
 }
 
 # Django production deployment settings
-# CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", cast=bool)
-# SECURE_HSTS_SECONDS = config("SECURE_HSTS_SECONDS", cast=bool)
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = config("SECURE_HSTS_INCLUDE_SUBDOMAINS", cast=bool)
-# SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", cast=bool)
-# SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", cast=bool)
-# SECURE_HSTS_PRELOAD = config("SECURE_HSTS_PRELOAD", cast=bool)
-# USE_X_FORWARDED_HOST = config("USE_X_FORWARDED_HOST", cast=bool)
+CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", cast=bool)
+SECURE_HSTS_SECONDS = config("SECURE_HSTS_SECONDS", cast=bool)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = config("SECURE_HSTS_INCLUDE_SUBDOMAINS", cast=bool)
+SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", cast=bool)
+SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", cast=bool)
+SECURE_HSTS_PRELOAD = config("SECURE_HSTS_PRELOAD", cast=bool)
+USE_X_FORWARDED_HOST = config("USE_X_FORWARDED_HOST", cast=bool)
