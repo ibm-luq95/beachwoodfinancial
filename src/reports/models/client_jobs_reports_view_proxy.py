61bdf8435901483ce86737553c58d0526d52663f
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-11-12 18:18:28 +0300

    delete not needed proxy model

--- src/reports/models/client_jobs_reports_view_proxy.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-#
-from reports.models import ClientJobsReportsDBView
-
-
-class ClientJobsReportsDBViewProxy(ClientJobsReportsDBView):
-    class Meta:
-        proxy = True
-        managed = False
