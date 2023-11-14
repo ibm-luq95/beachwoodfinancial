c33c8ee770069867761aa859c378f376c266c891
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-11-14 17:06:55 +0300

    refact: create cron field mixin, and add index to some fields

--- src/special_assignment/models/special_assignment.py
+++ src/special_assignment/models/special_assignment.py
@@ -12,6 +12,7 @@ from core.models.mixins import (
     TeamMembersMixin,
     StrModelMixin,
 )
+from core.models.mixins.cron_column_mixin import CronColumnMixin
 from core.utils import FileValidator
 from job.models import JobProxy
 from special_assignment.models.manager import SpecialAssignmentsManager
@@ -20,7 +21,7 @@ file_validator = FileValidator(max_size=1024 * 1000, content_types=IMAGES_AND_DO
 
 
 class SpecialAssignment(
-    BaseModelMixin, StartAndDueDateMixin, TeamMembersMixin, StrModelMixin
+    BaseModelMixin, StartAndDueDateMixin, TeamMembersMixin, CronColumnMixin, StrModelMixin
 ):
     client = models.ForeignKey(
         to=ClientProxy,
@@ -28,6 +29,7 @@ class SpecialAssignment(
         related_name="special_assignments",
         null=True,
         blank=True,
+        db_index=True,
     )
     job = models.ForeignKey(
         to=JobProxy,
@@ -35,6 +37,7 @@ class SpecialAssignment(
         related_name="special_assignments",
         null=True,
         blank=True,
+        db_index=True,
     )
     title = models.CharField(_("title"), max_length=100, null=False, blank=False)
     body = models.TextField(_("body"))
@@ -43,6 +46,7 @@ class SpecialAssignment(
         max_length=15,
         choices=SpecialAssignmentStatusEnum.choices,
         default=SpecialAssignmentStatusEnum.NOT_STARTED,
+        db_index=True,
     )
     attachment = models.FileField(
         _("attachment"),
@@ -52,7 +56,7 @@ class SpecialAssignment(
         validators=[file_validator],
     )
     notes = models.TextField(_("notes"), null=True, blank=True)
-    is_seen = models.BooleanField(_("is seen"), default=False)
+    is_seen = models.BooleanField(_("is seen"), default=False, db_index=True)
     assigned_by = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -60,7 +64,8 @@ class SpecialAssignment(
         help_text=_("readonly, you cant modified it"),
         editable=False,
         null=True,
-        blank=True
+        blank=True,
+        db_index=True,
     )
 
     objects = SpecialAssignmentsManager()
