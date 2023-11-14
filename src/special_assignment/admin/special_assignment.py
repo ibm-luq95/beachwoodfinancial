dd747c8104294e10449ea531c45d7599d92744f5
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-11-14 17:07:08 +0300

    fix: add list_display

--- src/special_assignment/admin/special_assignment.py
+++ src/special_assignment/admin/special_assignment.py
@@ -7,4 +7,16 @@ from special_assignment.models import SpecialAssignmentProxy
 
 @admin.register(SpecialAssignmentProxy)
 class SpecialAssignmentAdmin(BWBaseAdminModelMixin):
-    pass
+    list_filter = ("status", "client", "job", "updated_by_cron")
+    list_display = (
+        "title",
+        "client",
+        "job",
+        "manager",
+        "assigned_by",
+        "status",
+        "is_seen",
+        "attachment",
+        "updated_by_cron",
+        "created_at",
+    )
