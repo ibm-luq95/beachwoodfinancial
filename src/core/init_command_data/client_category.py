fd56d18bb394aee59d0f73e1f00bc8a7bfb85f14
Ibrahim Luqman <ibm.jnk@yandex.com>
2023-11-24 15:07:38 +0300

    new commands

---
+++ src/core/init_command_data/client_category.py
@@ -0,0 +1,15 @@
+from django.utils.translation import gettext as _
+
+INIT_DATA_CLIENT_CATEGORIES = [
+    _("Small Businesses"),
+    _("Freelancers and Solopreneurs"),
+    _("Nonprofit Organizations"),
+    _("Professional Service Firms"),
+    _("Government"),
+    _("Retailers"),
+    _("Real Estate Agencies"),
+    _("Healthcare Providers"),
+    _("Construction Companies"),
+    _("Educational Institutions"),
+    _("Technology Startups"),
+]
