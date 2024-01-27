693da2b3674b2d64bd42574e725ff082cf89f93d
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-01-27 15:52:19 +0300

    fix: add job year and month period fields

--- src/reports/filters/client/jobs_filter.py
+++ src/reports/filters/client/jobs_filter.py
@@ -9,6 +9,8 @@ from beach_wood_user.models import BWUser
 from client.models import ClientProxy
 from client_category.models import ClientCategory
 from core.choices import JobStatusEnum, JobTypeEnum, JobStateEnum
+from core.choices.fiscal_year import FiscalYearEnum
+from core.choices.months import MonthChoices
 from core.forms.mixins.base_form_mixin import BWBaseFormMixin
 from core.models.querysets import BaseQuerySetMixin
 from job_category.models import JobCategory
@@ -37,6 +39,7 @@ class ClientJobsFilter(
         self.fields["created_year"].help_text = _("Job created year")
         self.fields.pop("quick_created_at")
         self.fields.pop("created_at")
+        # self.fields.pop("created_year")
 
     clients = forms.ModelMultipleChoiceField(
         label=_("Client"),
@@ -51,6 +54,18 @@ class ClientJobsFilter(
         # widget=forms.CheckboxSelectMultiple,
         required=False,
         help_text=_("Client categories to filter"),
+    )
+    period_year = forms.ChoiceField(
+        label=_("Period year"),
+        required=False,
+        choices=FiscalYearEnum.choices,
+        help_text=_("Period Year"),
+    )
+    period_month = forms.ChoiceField(
+        label=_("Period month"),
+        required=False,
+        choices=MonthChoices.choices,
+        help_text=_("Period Month"),
     )
 
     job_categories = forms.ModelMultipleChoiceField(
