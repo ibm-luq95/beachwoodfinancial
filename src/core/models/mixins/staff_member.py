cca8409d10583e819f4832843b7c7c9330e2a59f
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-02-03 18:10:54 +0300

    style: reformat the code

--- src/core/models/mixins/staff_member.py
+++ src/core/models/mixins/staff_member.py
@@ -10,40 +10,40 @@ from core.models.querysets import BaseQuerySetMixin
 
 
 class StaffMemberMixin(models.Model):
-    user = models.OneToOneField(
-        to=settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="%(class)s",
-        null=True,
-        blank=True,
-    )
-    profile = models.OneToOneField(
-        to=Profile,
-        on_delete=models.CASCADE,
-        related_name="%(class)s",
-        null=True,
-        blank=True,
-    )
+	user = models.OneToOneField(
+		to=settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE,
+		related_name="%(class)s",
+		null=True,
+		blank=True,
+	)
+	profile = models.OneToOneField(
+		to=Profile,
+		on_delete=models.CASCADE,
+		related_name="%(class)s",
+		null=True,
+		blank=True,
+	)
 
-    def __str__(self) -> str:
-        # return f"Assistant - {self.user.first_name} {self.user.last_name}"
-        return f"{self.user.fullname}"
+	def __str__(self) -> str:
+		# return f"Assistant - {self.user.first_name} {self.user.last_name}"
+		return f"{self.user.fullname}"
 
-    class Meta:
-        ordering = ["user__first_name"]
-        abstract = True
+	class Meta:
+		ordering = ["user__first_name"]
+		abstract = True
 
-    @property
-    def is_active_labeled(self) -> str:
-        if self.user.is_active is True:
-            return _("Active")
-        else:
-            return _("Deactivate")
+	@property
+	def is_active_labeled(self) -> str:
+		if self.user.is_active is True:
+			return _("Active")
+		else:
+			return _("Deactivate")
 
-    def get_not_seen_special_assignments(self) -> BaseQuerySetMixin | None:
-        if hasattr(self, "special_assignments"):
-            return self.special_assignments.filter(
-                Q(is_seen=False) & ~Q(status__in=[CON_ARCHIVED])
-            )
-        else:
-            return None
+	def get_not_seen_special_assignments(self) -> BaseQuerySetMixin | None:
+		if hasattr(self, "special_assignments"):
+			return self.special_assignments.filter(
+				Q(is_seen=False) & ~Q(status__in=[CON_ARCHIVED])
+			)
+		else:
+			return None
