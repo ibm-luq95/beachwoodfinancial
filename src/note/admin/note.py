c0e0c95b3bde21aff204751f21bd090af4d66130
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-02-28 18:42:52 +0300

    fix: show new columns

--- src/note/admin/note.py
+++ src/note/admin/note.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-#
 from django.contrib import admin
-
+from django.utils.html import format_html, mark_safe
 from note.models import Note
 from core.admin import BWBaseAdminModelMixin
 
 
 @admin.register(Note)
 class NoteAdmin(BWBaseAdminModelMixin):
-    pass
+
+    list_display = [
+        "title",
+        "note_section",
+        "status",
+        "client",
+        "job",
+        "task",
+        "get_multiple_sections",
+        "created_at",
+    ]
+    list_filter = ["note_section", "status", "client", "job", "task"]
+
+    def get_multiple_sections(self, obj: Note) -> str:
+        return str(obj.get_related_objects_as_dict)
+        # return format_html(f'<pre><code class="language-json">{obj.get_multiple_sections}</code></pre>')
