ab79586a534e8fb4f46b1e07eca757a499fe35d4
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-03-16 22:11:29 +0300

    new: add briefcase urls

--- src/staff_briefcase/urls/__init__.py
+++ src/staff_briefcase/urls/__init__.py
@@ -5,6 +5,5 @@ app_name = "staff_briefcase"
 
 urlpatterns = [
     path("", include("staff_briefcase.urls.staff_briefcase"), name="briefcase"),
-    path("notes", include("staff_briefcase.urls.notes"), name="notes"),
-    path("documents", include("staff_briefcase.urls.documents"), name="documents"),
+    path("documents/", include("staff_briefcase.urls.documents"), name="documents"),
 ]
