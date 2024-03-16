c8a0e81cfcbf9da28f6a347b1b06f08d1db3cfff
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-03-16 22:12:19 +0300

    new: create serializers for briefcase

---
+++ src/staff_briefcase/serializers/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-#
+from .briefcase import StaffBriefcaseSerializer
+from .notes import StaffNotesSerializer
+from .documents import StaffDocumentsSerializer
+from .accounts import StaffAccountsSerializer
---
+++ src/staff_briefcase/serializers/accounts.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-#
+import traceback
+
+from django.db import transaction
+from rest_framework import serializers
+
+from core.constants import EXCLUDED_FIELDS
+from core.utils.developments.debugging_print_object import BWDebuggingPrint
+from staff_briefcase.models import StaffAccounts, StaffBriefcase
+
+
+class StaffAccountsSerializer(serializers.ModelSerializer):
+    briefcase = serializers.UUIDField(required=True)
+
+    class Meta:
+        model = StaffAccounts
+        fields = ("title", "url", "briefcase", "username_email", "password", "name")
+        depth = 1
+
+    def create(self, validated_data):
+        try:
+            BWDebuggingPrint.pprint(validated_data)
+            with transaction.atomic():
+                briefcase_pk = validated_data.pop("briefcase")
+                briefcase_obj = StaffBriefcase.objects.get(pk=briefcase_pk)
+                obj = StaffAccounts.objects.create(**validated_data)
+                # BWDebuggingPrint.pprint(obj.password)
+                briefcase_obj.accounts.add(obj)
+                briefcase_obj.save()
+                # obj.save(foo=validated_data["foo"])
+                return obj
+        except Exception as ex:
+            print(traceback.format_exc())
