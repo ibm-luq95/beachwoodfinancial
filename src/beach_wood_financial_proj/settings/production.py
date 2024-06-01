ebf8dd16869e998bceb08872e27098d889e3763a
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-06-01 17:34:26 +0300

    fix: add some security settings

--- src/beach_wood_financial_proj/settings/production.py
+++ src/beach_wood_financial_proj/settings/production.py
@@ -2,6 +2,32 @@ from .base import *
 
 DEBUG = config("DEBUG", cast=bool)
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        # Checks the similarity between the password and a set of attributes of the user.
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "OPTIONS": {
+            "user_attributes": ("email", "first_name", "last_name"),
+            "max_similarity": 0.7,
+        },
+    },
+    {
+        # Checks whether the password meets a minimum length.
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 8,
+        },
+    },
+    {
+        # Checks whether the password occurs in a list of common passwords
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        # Checks whether the password isn’t entirely numeric
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
 # Database configurations
 DATABASES = {
     "default": {
@@ -23,3 +49,9 @@ SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", cast=bool)
 SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", cast=bool)
 SECURE_HSTS_PRELOAD = config("SECURE_HSTS_PRELOAD", cast=bool)
 USE_X_FORWARDED_HOST = config("USE_X_FORWARDED_HOST", cast=bool)
+
+# OWSP recommendation security configs
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+# SECURE_PROXY_SSL_HEADER = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
