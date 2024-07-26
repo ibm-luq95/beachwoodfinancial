abb4298c49c3755c32a2ff1feeb2b0c9fa890f74
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-07-26 17:25:57 +0300

    fix: add stage config

--- src/beach_wood_financial_proj/settings/__init__.py
+++ src/beach_wood_financial_proj/settings/__init__.py
@@ -1,10 +1,21 @@
 from pathlib import Path
 
 from decouple import Config, RepositoryEnv
+import configparser
 
 cwd = Path.cwd()
-# env_path = cwd / ".env" / ".env"
-env_path = cwd / ".env" / ".env_docker"  # For docker
+config = configparser.RawConfigParser()
+stage_env_file = cwd / ".env" / "current_stage.ini"
+if stage_env_file.exists() is False:
+    raise Exception("stage file not exists!!!".upper())
+config.read(stage_env_file)
+stage = config.get("environment", "STAGE_ENVIRONMENT")
+# print(stage)
+if stage == "DOCKER":
+    env_path = cwd / ".env" / ".env_docker"  # For docker
+elif stage == "LOCAL_DEV":
+    env_path = cwd / ".env" / ".env"  # For local
+
 
 # check if .env file not exists
 if env_path.exists() is False:
