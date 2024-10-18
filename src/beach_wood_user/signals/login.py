6b5fff339dfe50e415cdc9ea36e9b0c93ac571fd
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-10-18 16:17:07 +0300

    fix: Disable login signal

--- src/beach_wood_user/signals/login.py
+++ src/beach_wood_user/signals/login.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-#
-from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import update_last_login
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from django.utils import timezone
-from django.contrib.auth.models import update_last_login
-
-from core.utils.developments.debugging_print_object import BWDebuggingPrint
 
 
 @receiver(user_logged_in)
@@ -15,11 +11,10 @@ def log_user_login(sender, request, user, **kwargs):
     user.last_login = timezone.now()
     # user.save(update_fields=["last_login"])
     user.save()
-    BWDebuggingPrint.pprint("Update last login done")
 
 
-user_logged_in.disconnect(update_last_login, dispatch_uid="update_last_login")
-user_logged_in.disconnect(user_logged_in, dispatch_uid="user_logged_in")
+# user_logged_in.disconnect(update_last_login, dispatch_uid="update_last_login")
+# user_logged_in.disconnect(user_logged_in, dispatch_uid="user_logged_in")
 # user_logged_in.disconnect(
 #     update_last_login, sender=settings.AUTH_USER_MODEL, dispatch_uid="update_last_login"
 # )
