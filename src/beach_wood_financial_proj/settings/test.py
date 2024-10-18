219d46412d876ef1a2f2a1afda868d4f2b1dae75
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-10-18 16:09:41 +0300

    dev: fix issue django debug toolbar for stage, and fix STORAGES

--- src/beach_wood_financial_proj/settings/test.py
+++ src/beach_wood_financial_proj/settings/test.py
@@ -1,13 +1,29 @@
+import logging
+# from .base import *
 from .dev import *
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        "TEST": {
-            "NAME": BASE_DIR / "mytestdatabase.sqlite3",
-            "ENGINE": "django.db.backends.sqlite3",
-        },
-    },
+logging.disable()
 
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+# DEBUG = True
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+    "IS_RUNNING_TESTS": True,
+    "RENDER_PANELS": None,
+}
+
+# DATABASES["TEST"] = {
+#     "ENGINE": "django.db.backends.mysql",
+#     "NAME": "DB_NAME",
+#     "USER": "DB_USER",
+#     "PASSWORD": "DB_PASSWORD",
+#     "HOST": "localhost",  # Or an IP Address that your DB is hosted on
+#     "PORT": "3306",
+# }
+
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.InMemoryStorage"},
+    "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"},
 }
