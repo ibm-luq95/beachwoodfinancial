f5416969693e9bcabfdfa9c69ddfc4078cdcfcca
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-12-24 15:34:13 +0300

    fix: fix resource object

--- src/job_category/management/commands/export_job_category.py
+++ src/job_category/management/commands/export_job_category.py
@@ -2,14 +2,14 @@
 from django.utils.translation import gettext as _
 
 from core.management.mixins.export_mixin import ExportingCommandMixin
-from document.resources.document import DocumentResource
+from job_category.resources.job_category import JobCategoryResource
 
 
 class Command(ExportingCommandMixin):
     def __init__(self, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
-        self.resources_object = DocumentResource()
+        self.resources_object = JobCategoryResource()
         self.app_name = "job_category"
         self.file_name = "JobCategory"
         self.help = _("Export job category for backup")
