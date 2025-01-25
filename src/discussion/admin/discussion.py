5e01b6fca27b2c10f6381798289f369166e90fa1
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-01-25 18:24:18 +0300

    fix: improve adminsite

--- src/discussion/admin/discussion.py
+++ src/discussion/admin/discussion.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-#
+import textwrap
+
 from django.contrib import admin
 
 from core.admin import BWBaseAdminModelMixin
@@ -7,4 +9,18 @@ from discussion.models import DiscussionProxy
 
 @admin.register(DiscussionProxy)
 class DiscussionAdmin(BWBaseAdminModelMixin):
-    pass
+    list_display = [
+        "body",
+        "attachment",
+        "has_replies",
+        "special_assignment",
+        "job",
+        "created_at",
+    ]
+    readonly_fields = ["is_seen"]
+
+    @admin.display(description="Has Replies")
+    def has_replies(self, obj: DiscussionProxy):
+        if obj.replies:
+            return textwrap.shorten(obj.replies.body, width=50, placeholder="...")
+        return None
