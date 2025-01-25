e32f6cc9d707dddb5a1976239b229b7803f6f32e
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-01-25 18:21:06 +0300

    fix: improve adminsite

--- src/client_category/admin/category.py
+++ src/client_category/admin/category.py
@@ -7,4 +7,8 @@ from core.admin import BWBaseAdminModelMixin
 
 @admin.register(ClientCategory)
 class ClientCategoryAdmin(BWBaseAdminModelMixin):
-    pass
+    list_display = ["name", "get_clients_count", "created_at"]
+
+    @admin.display(description="Clients")
+    def get_clients_count(self, obj: ClientCategory):
+        return obj.clients.count()
