12906bb32bcbfaf42af173d4dcdef6795684867c
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-01-25 18:22:23 +0300

    no changes

--- src/special_assignment/views/api.py
+++ src/special_assignment/views/api.py
@@ -6,6 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.api.permissions import BaseApiPermissionMixin
 from core.utils import get_formatted_logger, debugging_print
+from core.utils.developments.debugging_print_object import DebuggingPrint
 from special_assignment.models import SpecialAssignmentProxy
 from special_assignment.serializers import SpecialAssignmentSerializer
 
@@ -18,3 +19,12 @@ class SpecialAssignmentViewSet(ModelViewSet):
     parser_classes = [parsers.FormParser, parsers.MultiPartParser]
     perm_slug = "special_assignment.specialassignment"
     queryset = SpecialAssignmentProxy.objects.all()
+
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     # serializer.is_valid(raise_exception=True)
+    #     serializer.is_valid(raise_exception=False)
+    #     DebuggingPrint.print(serializer.data)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
