96b9152fbf4c73cd6157a88fab9f5dc685eb8370
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-01-25 18:22:50 +0300

    fix: add resource class, and discussion inlineform

--- src/special_assignment/admin/special_assignment.py
+++ src/special_assignment/admin/special_assignment.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-#
+from django import forms
 from django.contrib import admin
 
 from core.admin import BWBaseAdminModelMixin
+from discussion.models import DiscussionProxy
 from special_assignment.models import SpecialAssignmentProxy
+from special_assignment.resources.special_assignment import SpecialAssignmentResource
+
+
+class DiscussionInline(admin.TabularInline):
+    model = DiscussionProxy
+    fields = ["body", "attachment", "special_assignment"]
+    extra = 1
 
 
 @admin.register(SpecialAssignmentProxy)
 class SpecialAssignmentAdmin(BWBaseAdminModelMixin):
     list_filter = ("status", "client", "job", "updated_by_cron")
+    inlines = [DiscussionInline]
+    resource_classes = [SpecialAssignmentResource]
     list_display = (
         "title",
         "client",
