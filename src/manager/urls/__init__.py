ff7a9a083e005e9a3324b05e2214d085c5cc29a4
Ibrahim Luqman <ibm.jnk@yandex.com>
2025-02-02 14:19:58 +0300

    feat: add schedule jobs list URL to manager urlpatterns

--- src/manager/urls/__init__.py
+++ src/manager/urls/__init__.py
@@ -6,6 +6,7 @@ from manager.views import (
     ManagerCreateView,
     ManagerListView,
 )
+from manager.views.schedule_jobs import JobScheduleListView
 
 app_name = "managers"
 
@@ -14,5 +15,6 @@ urlpatterns = [
     path("create", ManagerCreateView.as_view(), name="create"),
     path("update/<uuid:pk>", ManagerUpdateView.as_view(), name="update"),
     path("delete/<uuid:pk>", ManagerDeleteView.as_view(), name="delete"),
+    path("schedule", JobScheduleListView.as_view(), name="schedule_jobs_list"),
     # path("<uuid:pk>", ManagerDetailsView.as_view(), name="details"),
 ]
