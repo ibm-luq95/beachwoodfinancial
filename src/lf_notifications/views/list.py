afb8c4b1872b5e5e91891ef0a63e10f382f2aac0
Ibrahim Luqman <ibm.jnk@yandex.com>
2026-07-25 16:11:33 +0300

    refactor: improve queryset handling and filtering logic in NotificationListView

--- src/lf_notifications/views/list.py
+++ src/lf_notifications/views/list.py
@@ -11,7 +11,7 @@ class NotificationListView(LoginRequiredMixin, ListView):
     paginate_by = 20
 
     def get_queryset(self):
-        return NotificationRecipientProxy.objects.filter(
+        qs = NotificationRecipientProxy.objects.filter(
             recipient=self.request.user
         ).select_related(
             "notification", 
@@ -23,3 +23,21 @@ class NotificationListView(LoginRequiredMixin, ListView):
             "notification__actor__assistant__profile",
             "notification__actor__cfo__profile",
         ).order_by("-created_at")
+
+        filter_param = self.request.GET.get("filter")
+        if filter_param == "unread":
+            qs = qs.filter(is_read=False)
+        elif filter_param == "discussion":
+            qs = qs.filter(notification__content_type__model__in=["discussion", "discussionproxy"])
+        elif filter_param == "job":
+            qs = qs.filter(notification__content_type__model__in=["job", "jobproxy", "specialassignment", "specialassignmentproxy"])
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        base_qs = NotificationRecipientProxy.objects.filter(recipient=self.request.user)
+        context["unread_count"] = base_qs.filter(is_read=False).count()
+        context["total_count"] = base_qs.count()
+        context["current_filter"] = self.request.GET.get("filter", "all")
+        return context
