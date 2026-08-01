2af142e0a974c567eb181eef53e3db0bb2650eeb
Ibrahim Luqman <ibm.jnk@yandex.com>
2026-08-01 17:16:00 +0300

    feat(api/mixins): add CachedViewSetResponseMixin for list caching

--- src/core/api/mixins/__init__.py
+++ src/core/api/mixins/__init__.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-#
+from .cached_viewset import CachedViewSetResponseMixin
 from .role_scoped_queryset import RoleScopedQuerysetMixin
+
+__all__ = ["RoleScopedQuerysetMixin", "CachedViewSetResponseMixin"]
