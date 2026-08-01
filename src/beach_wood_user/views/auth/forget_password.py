925500e662e7fdf6c7d42219b0a95e5a7b3c1522
Ibrahim Luqman <ibm.jnk@yandex.com>
2026-08-01 17:13:50 +0300

    feat: implement throttling in BWForgetPasswordView for enhanced security

--- src/beach_wood_user/views/auth/forget_password.py
+++ src/beach_wood_user/views/auth/forget_password.py
@@ -2,13 +2,16 @@
 from typing import Any
 from django.views.generic import TemplateView
 
+from core.api.throttling import AuthEndpointRateThrottle
 from core.utils import get_trans_txt
+from core.views.mixins import ThrottledViewMixin
 
 
-class BWForgetPasswordView(TemplateView):
+class BWForgetPasswordView(ThrottledViewMixin, TemplateView):
     """
     Provides users the ability to forget password view
     """
+    throttle_classes = [AuthEndpointRateThrottle]
     template_name = "beach_wood_user/auth/reset_password.html"
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
