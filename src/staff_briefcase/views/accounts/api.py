6a8379f7c1b235154745f0e3af97edadca225774
Ibrahim Luqman <ibm.jnk@yandex.com>
2026-08-01 17:23:16 +0300

    feat(staff_accounts): add filtering, searching, and ordering capabilities to StaffAccountsViewSet

--- src/staff_briefcase/views/accounts/api.py
+++ src/staff_briefcase/views/accounts/api.py
@@ -16,6 +16,10 @@ class StaffAccountsViewSet(ModelViewSet):
     serializer_class = StaffAccountsSerializer
     permission_classes = (permissions.IsAuthenticated, BaseApiPermissionMixin)
     perm_slug = "staff_briefcase.staffaccounts"
+    filterset_fields = ["account_name", "status", "is_deleted"]
+    search_fields = ["account_name", "account_email"]
+    ordering_fields = ["created_at", "updated_at", "account_name"]
+    ordering = ["account_name"]
     authentication_classes = [TokenAuthentication]
 
     def get_queryset(self):
