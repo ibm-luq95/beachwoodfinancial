85daaa183736536854181c0f3c3c2e4eec3cf0a0
Ibrahim Luqman <ibm.jnk@yandex.com>
2026-08-30 21:37:31 +0300

    refactor(discussion): update manager model and querysets

--- src/discussion/models/manager.py
+++ src/discussion/models/manager.py
@@ -5,7 +5,9 @@ from discussion.models.queryset import DiscussionQuerySet
 
 class RepliesManager(SoftDeleteManager):
     def get_queryset(self) -> DiscussionQuerySet:
-        queryset = DiscussionQuerySet(self.model, using=self._db).filter(is_deleted=False)
+        queryset = DiscussionQuerySet(self.model, using=self._db).filter(
+            is_deleted=False
+        )
         return queryset
 
     def get_only_discussions_without_replies(self) -> DiscussionQuerySet:
