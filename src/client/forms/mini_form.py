74e85fbf03274521132de6b19cb6ec222f2c1023
Ibrahim Luqman <ibm.jnk@yandex.com>
2026-08-30 21:37:28 +0300

    refactor(client): update mini_form form validation and rendering

--- src/client/forms/mini_form.py
+++ src/client/forms/mini_form.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-#
 from typing import Optional
-from django import forms
 
-from client_category.models import ClientCategory
+from django import forms
 from django.utils.translation import gettext as _
 
+from client_category.models import ClientCategory
 from core.constants.file_types_validation import IMAGES_FT
 from core.forms.mixins.base_form_mixin import BWBaseFormMixin
 from core.forms.mixins.js_modal_form_renderer_mixin import BWJSModalFormRendererMixin
@@ -12,11 +11,12 @@ from core.forms.mixins.remove_fields_mixin import RemoveFieldsMixin
 from core.utils import FileValidator
 from important_contact.models import ImportantContact
 
+
 file_validator = FileValidator(max_size=1024 * 1000, content_types=IMAGES_FT)
 
 
 class ClientMiniForm(RemoveFieldsMixin, BWBaseFormMixin):
-    def __init__(self, removed_fields: Optional[list] = None, *args, **kwargs):
+    def __init__(self, removed_fields: list | None = None, *args, **kwargs):
         super(BWBaseFormMixin, self).__init__(*args, **kwargs)
         RemoveFieldsMixin.__init__(self, removed_fields=removed_fields)
 
